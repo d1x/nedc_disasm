6d2a7e5945d76238f9a37c955edb6f3f2d0173fe
d1x <d1x.github@gmail.com>
2019-07-27 00:02:55 +0100

    Detect invalid opcodes.

--- src/Disasm.js
+++ src/Disasm.js
@@ -2,7 +2,31 @@
 const START_ADDR = 0x100;
 
 const OPCODE_TABLE = {
-  0x0: 'nop'
+  0x0: 'nop',
+  0x08: null,
+  0x27: null,
+  0xcb: null,
+  0xd7: null,
+  0xd9: null,
+  0xdb: null,
+  0xdd: null,
+  0xdf: null,
+  0xe0: null,
+  0xe2: null,
+  0xe4: null,
+  0xe7: null,
+  0xe8: null,
+  0xea: null,
+  0xec: null,
+  0xed: null,
+  0xef: null,
+  0xf3: null,
+  0xf4: null,
+  0xf7: null,
+  0xfb: null,
+  0xfc: null,
+  0xfd: null,
+  0xff: null
 };
 
 class Disasm {
@@ -14,6 +38,33 @@ class Disasm {
     this.map = {};
     /** @type number */
     this.addr = START_ADDR;
+  }
+
+  /**
+   * @param {string} opcode
+   * @private
+   */
+  static unsupported_(opcode) {
+    return `e-Reader unsupported opcode: ${Disasm.toByteString(opcode)}`;
+  }
+
+  /**
+   * @param {string} decimal
+   * @return {string} hex address 0x0000 to 0xffff
+   * @private
+   */
+  static toAddress_(decimal) {
+    const hex = parseInt(decimal).toString(16);
+    return `0x${'0000'.substr(0, 4 - hex.length)}${hex}`;
+  }
+
+  /**
+   * @param {string} decimal
+   * @return {string} hex address 0x00 to 0xff
+   */
+  static toByteString(decimal) {
+    const hex = parseInt(decimal).toString(16);
+    return `0x${'00'.substr(0, 2 - hex.length)}${hex}`;
   }
 
   /** @param {Uint8Array} uint8array */
@@ -28,9 +79,13 @@ class Disasm {
     const queue = [];
     let nextByte = 0;
     queue.push(this.input[nextByte]);
+
     while (queue.length !== 0) {
-      let curr = queue.shift();
-      this.map[this.addr] = OPCODE_TABLE[curr];
+      const byte = queue.shift();
+      if (OPCODE_TABLE[byte] === null) {
+        return Disasm.unsupported_(byte);
+      }
+      this.map[this.addr] = OPCODE_TABLE[byte];
       if (++nextByte < this.input.length) {
         queue.push(this.input[nextByte]);
       }
@@ -48,16 +103,6 @@ class Disasm {
     lines.sort();
     lines.forEach((line) => output.push(`${Disasm.toAddress_(line)}    ${this.map[line]}`));
     return output.join('\n');
-  }
-
-  /**
-   * @param {string} decimal
-   * @return {string} hex address 0x0000 to 0xffff
-   * @private
-   */
-  static toAddress_(decimal) {
-    const hex = parseInt(decimal).toString(16);
-    return `0x${'0000'.substr(0, 4 - hex.length)}${hex}`;
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -1,19 +1,27 @@
 import {expect} from 'chai';
-import {describe, before} from 'mocha';
+import {describe, beforeEach} from 'mocha';
 import Disasm from '../src/Disasm';
 
 describe('Opcodes', () => {
 
   let disasm;
 
-  before('', () => {
+  beforeEach('', () => {
     disasm = new Disasm();
   });
 
   it('should disassemble nop', () => {
-    const input = new Uint8Array(1);
-    input[0] = 0;
-    disasm.setUint8Array(input);
+    disasm.setUint8Array(new Uint8Array([0x00]));
     expect(disasm.disassemble()).to.equal('0x0100    nop');
+  });
+
+  it('should detect invalid opcodes', () => {
+    const invalidOpcodes = [0x08, 0x27, 0xcb, 0xd7, 0xd9, 0xdb, 0xdd, 0xdf,
+      0xe0, 0xe2, 0xe4, 0xe7, 0xe8, 0xea, 0xec, 0xed, 0xef, 0xf3, 0xf4, 0xf7,
+      0xfb, 0xfc, 0xfd, 0xff];
+    invalidOpcodes.forEach((opcode) => {
+      disasm.setUint8Array(new Uint8Array([].concat(opcode)));
+      expect(disasm.disassemble()).to.equal(`e-Reader unsupported opcode: ${Disasm.toByteString(opcode)}`);
+    });
   });
 });
