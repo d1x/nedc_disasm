cc31202a016bb46b69ed3b1a04217ce38d35de22
d1x <d1x.github@gmail.com>
2019-07-31 22:42:52 +0100

    Test program flow

---
+++ test/flow.js
@@ -0,0 +1,23 @@
+import {expect} from 'chai';
+import {describe, beforeEach} from 'mocha';
+import Disasm from '../src/Disasm';
+
+describe('Program flow', () => {
+  'use strict';
+  let disasm;
+
+  beforeEach('', () => {
+    disasm = new Disasm();
+  });
+
+  it('should disassemble nops', () => {
+    disasm.setUint8Array(new Uint8Array([0x00, 0x00, 0x00, 0x00, 0x00, ]));
+    expect(disasm.disassemble()).to.equal([
+      '0x0100    nop',
+      '0x0101    nop',
+      '0x0102    nop',
+      '0x0103    nop',
+      '0x0104    nop',]
+      .join('\n'));
+  });
+});
